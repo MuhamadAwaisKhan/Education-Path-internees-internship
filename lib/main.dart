d8f9628c88c20dba69ac702a757946e824613d68
MuhamadAwaisKhan <103516687+MuhamadAwaisKhan@users.noreply.github.com>
2024-07-06 15:32:44 +0500

    project last Summer break commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:internee/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:internee/homescreen.dart';
 import 'package:internee/splashscreen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +11,8 @@ void main() async {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
 
-    home:SplashScreen(),
+    home:HomeScreen(),
+    // SplashScreen(),
 
   ));
 }
